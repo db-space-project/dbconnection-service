363d1628960b5d13511cd6b5a8abceeb88606ab6
nirajgeorgian <nirajgeorgian@gmail.com>
2020-10-31 01:44:04 +0530

    empty spring project initialized

---
+++ src/main/java/com/dbspaceproject/dbconnection/DbConnectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dbspaceproject.dbconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConnectionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbspaceproject/dbconnection/DbConnectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbspaceproject.dbconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConnectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
